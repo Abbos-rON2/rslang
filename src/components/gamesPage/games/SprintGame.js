2f0d4c0e9a4c9681024faa243990cc7f6b0829f1
Yuliya <yulia.podd@gmail.com>
2021-04-03 23:24:19 +0300

    feat: add comparison of user's answer

--- src/components/gamesPage/games/SprintGame.js
+++ src/components/gamesPage/games/SprintGame.js
@@ -1,26 +1,37 @@
-import { Fragment } from "react";
 import "../../../styles/sprint_styles.css";
 import { CheckCircleTwoTone } from "@ant-design/icons";
 import "antd/dist/antd.css";
 import { Button } from "antd";
-
+import { Fragment, useEffect, useState } from "react";
 export default function SprintGame(props) {
-  let randomWord, randomTranslation;
-  
-  getRandomWordAndTranslation();
+  const [currentWord, setCurrentWord] = useState(props.words[0].word);
+  const [currentTranslation, setCurrentTranslation] = useState(
+    props.words[0].wordTranslate
+  );
+  const [expected, setExpected] = useState(true);
 
   function getRandomWordAndTranslation() {
     const randomNum = Math.round(Math.random());
     const randomIndex = Math.floor(Math.random() * 21);
 
-    randomWord = props.words[randomIndex].word;
-    
-    randomNum === 0 ? randomTranslation = props.words[randomIndex + 1].wordTranslate : randomTranslation = props.words[randomIndex].wordTranslate;
-    console.log(randomNum, randomIndex, randomWord, randomTranslation);
+    setCurrentWord(props.words[randomIndex].word);
+
+    if (randomNum === 0) {
+      setCurrentTranslation(props.words[randomIndex + 1].wordTranslate);
+      setExpected(false);
+    } else {
+      setCurrentTranslation(props.words[randomIndex].wordTranslate);
+      setExpected(true);
+    }
+    console.log(randomNum, randomIndex, currentWord, currentTranslation);
   }
 
-
-  function isCorrect() {}
+  function isCorrect(answer) {
+    answer === expected
+      ? console.log("you are right!")
+      : console.log("you are wrong");
+      getRandomWordAndTranslation();
+  }
   return (
     <Fragment>
       <div>Спринт</div>
@@ -39,19 +50,23 @@ export default function SprintGame(props) {
           </section>
           <section className="words_section">
             <div className="words_wrapper">
-              <div>{randomWord}</div>
-              <div>{randomTranslation}</div>
+              <div>{currentWord}</div>
+              <div>{currentTranslation}</div>
             </div>
           </section>
           <section className="buttons_section">
-            <Button type="primary" id="button_ok" onClick={() => this.isCorrect(true)}>
+            <Button
+              type="primary"
+              id="button_ok"
+              onClick={() => isCorrect(true)}
+            >
               Верно
             </Button>
             <Button
               type="primary"
               danger
               id="button_wrong"
-              onClick={() => this.isCorrect(false)}
+              onClick={() => isCorrect(false)}
             >
               Неверно
             </Button>
