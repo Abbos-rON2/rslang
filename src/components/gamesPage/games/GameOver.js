f7bf43f794590aadfa108971e3ef7ecee34ea2b0
Yuliya <yulia.podd@gmail.com>
2021-04-13 17:41:10 +0300

    refactor: change buttons in modal

--- src/components/gamesPage/games/GameOver.js
+++ src/components/gamesPage/games/GameOver.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "antd/dist/antd.css";
-import { Modal } from "antd";
+import { Modal, Button } from "antd";
 
 export default function GameOver({ points, gameOver }) {
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -9,20 +9,25 @@ export default function GameOver({ points, gameOver }) {
     gameOver ? setIsModalVisible(true) : setIsModalVisible(false);
   }, [gameOver]);
 
-  const handleOk = () => {
+  function handleOk () {
     setIsModalVisible(false);
   };
 
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
   return (
     <>
       <Modal
         title="Basic Modal"
         visible={isModalVisible}
         onOk={handleOk}
-        onCancel={handleCancel}
+        footer={[
+          <Button
+            key="submit"
+            type="primary"
+            onClick={handleOk}
+          >
+            Ok
+          </Button>,
+        ]}
       >
         <p>Game Over</p>
         <div>Total points: {points}</div>
