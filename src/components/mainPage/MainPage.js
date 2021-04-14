75036ceb7c4ce3097127c69a0e3262930f1fe95f
Yuliya <yulia.podd@gmail.com>
2021-04-14 21:59:12 +0300

    feat: add team info

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -1,13 +1,33 @@
 import Video from "./Video";
 import "animate.css/animate.css";
+import {teamInfo} from "../../constants/constants";
+import {GithubOutlined } from "@ant-design/icons"
 
 export default function MainPage() {
-  return (
+  const memberOfTeam = teamInfo.map(({name, photo, text, link}) => {
+    return (
+      <div
+        className="member_div"
+        id={name}
+        key={name}
+        >
+           <img src={photo}></img>
+     <div className="member_info">
+      
+     <div>{name} <a href={link}><GithubOutlined /></a></div>
+        <div>{text}</div>
+        
+     </div>
+       
+      </div>
+    );
+  });
+    return (
     <div className="main_page_wrapper">
       <section className="main_page_about">
        
         <div className="about_text animate__animated animate__backInDown">
-        <h1>Welcome to RS Lang!</h1>
+        <h1 className="h1">Welcome to RS Lang!</h1>
         <p>
           RS Lang - это уникальное приложение для изучения английского.
           Увлекательные игры для тренировки слов и метод интервального
@@ -19,7 +39,11 @@ export default function MainPage() {
           </div>
       </section>
       <section className="main_page_video"><Video></Video></section>
-      <section className="main_page_team">team - to do</section>
+      <section className="main_page_team">
+      <h3>Наша команда</h3>
+      <div className="team_wrapper">{memberOfTeam} </div>
+     
+      </section>
     </div>
   );
 }
